bef3a74fe91a4e193f0db778326092767d10dfb1
Latif İhsan BULUT <45788785+latifihsanbulut@users.noreply.github.com>
2021-04-16 19:03:10 +0300

    upfate

---
+++ DividePDF/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DividePDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(129, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 77);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(322, 58);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(326, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PDF BÖL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçilen Dosya";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(129, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PDF bölme işlemi başarılı";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 180);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Divide Pdf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DividePDF/Form1.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+
+namespace DividePDF
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog chooseFile = new OpenFileDialog();
+        private string filePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            chooseFile.Multiselect = false;
+            chooseFile.Filter = "Pdf files (*.pdf)|*.pdf";
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            filePath = files[files.Length-1];
+            WriteFileName();
+        }       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chooseFile.ShowDialog();
+            filePath = chooseFile.FileName;
+            WriteFileName();
+        }
+
+        private void WriteFileName()
+        {
+            string[] words = filePath.Split('\\');
+            textBox1.Text = words[words.Length - 1];
+            label2.Visible = false;
+            label2.Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (filePath.Trim().Equals(""))
+            {
+                MessageBox.Show("Lütfen bölmek istediğiniz pdf dosyasını seçin");
+                return;
+            }
+            PdfReader reader = new PdfReader(filePath);
+            int n = reader.NumberOfPages;
+            // step 1
+            iTextSharp.text.Rectangle mediabox = new iTextSharp.text.Rectangle(getHalfPageSize(reader.GetPageSizeWithRotation(1)));
+            Document document = new Document(mediabox);
+            // step 2
+            string saveFileName = DateTime.Now.ToFileTime() + ".pdf";
+            string savePath = Path.Combine(
+               Environment.GetFolderPath(Environment.SpecialFolder.Desktop), saveFileName);
+            PdfWriter writer
+                = PdfWriter.GetInstance(document, new FileStream(savePath, FileMode.Create));
+            // step 3
+            document.Open();
+            // step 4
+            PdfContentByte content = writer.DirectContent;
+            PdfImportedPage page;
+            int i = 1;
+            while (true)
+            {
+                page = writer.GetImportedPage(reader, i);
+                content.AddTemplate(page, 0, -mediabox.Height);
+                //document.NewPage(); /*sayfanın altındaki boş kısımı almamak için commentlendi*/
+                //content.AddTemplate(page, 0, 0);
+                if (++i > n)
+                    break;
+                mediabox = new iTextSharp.text.Rectangle(getHalfPageSize(reader.GetPageSizeWithRotation(i)));
+                document.SetPageSize(mediabox);
+                document.NewPage();
+            }
+            // step 5
+            document.Close();
+            reader.Close();
+
+            label2.Visible = true;
+            label2.Update();
+        }
+
+        public iTextSharp.text.Rectangle getHalfPageSize(iTextSharp.text.Rectangle pagesize)
+        {
+            float width = pagesize.Width;
+            float height = pagesize.Height;
+            return new iTextSharp.text.Rectangle(width, height / 2);
+        }
+    }
+    
+}
